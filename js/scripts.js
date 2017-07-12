111e1fbe6b5acbb09526186a26d42d5ad46562e8
Kim Schulze and Robert Murray <robert@>
2017-07-12 14:21:18 -0700

    tranform a word beginning with vowel or consonant into pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 var vowels = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+
 
 var wordToArray = function(word) {
   return word.split("");
-}
-
-var startsWithVowel = function(wordToArray(word)[0]) {
-  return isVowel()
-}
+};
 
 /* 1
 we need to turn the string into an array of letters
@@ -18,22 +16,53 @@ check for consonants until we hit a vowel
 */
 var isVowel = function(letter) {
   for (var i = 0; i < vowels.length; i++) {
-    if (letter == vowels[i]) {
+    if (letter === vowels[i]) {
       return true;
     }
   }
   return false;
-}
+};
 
-var translatePigLatin = function() {
+var isConsonant = function(letter) {
+  for (var i = 0; i < consonants.length; i++) {
+    if (letter === consonants[i]) {
+      return true
+    }
+  }
+  return false;
+};
 
-}
+var consonantCount = function(word) {
+  for (var i = 0; i < consonants.length; i++) {
+    if (!isConsonant(word[i])) {
+      return i;
+    }
+  }
+};
+
+var translatePigLatin = function(inputWord) {
+  var arrayWord = wordToArray(inputWord);
+  var firstLetter = arrayWord[0];
+
+  if (isVowel(firstLetter)) {
+    var vowelTranslation = arrayWord.concat("-way");
+    return vowelTranslation.join("");
+  }
+  else if (isConsonant(firstLetter)) {
+    var conTranslation = arrayWord.splice(1, arrayWord.length);
+    console.log(conTranslation);
+    conTranslation = conTranslation.concat("-" + firstLetter + "ay");
+    return conTranslation.join("");
+  }
+};
 
 $(function() {
   $("#pigLatin").submit(function(event) {
     event.preventDefault();
     var userInput = $("#sentence").val();
 
-    translatePigLatin(userInput);
+    var translate = translatePigLatin(userInput);
+
+    $("#output").text(translate);
   });
 });
